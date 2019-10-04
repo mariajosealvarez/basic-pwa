de5923fa6e2e33ba0d052419405d5644ad46a196
mariajosealvarez <mariajose.230985@gmail.com>
2019-10-04 16:21:27 -0300

    play sound

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads > div > div");
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener('click', () => {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+    });
+  });
+
+
+
+});
